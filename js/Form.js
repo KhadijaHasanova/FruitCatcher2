60522276629e1a6578a04b968a404a5b7690cd7d
KhadijaHasanova <74422542+KhadijaHasanova@users.noreply.github.com>
2021-02-19 08:13:10 -0600

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -4,7 +4,6 @@ class Form{
        this.button = createButton('Play');
        this.greeting = createElement('h2');
        this.title = createElement('h2');
-       this.reset = createButton("Reset");
     }
     hide() {
         this.greeting.hide();
@@ -14,20 +13,17 @@ class Form{
     }
     display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(350, 50);
+        this.title.position(650, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(550,400);
+        this.input.position(850,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(560,500);
+        this.button.position(860,500);
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
-
-        //position the reset button
-        this.reset.position(950,150);
 
         this.button.mousePressed(() => {
             this.input.hide();
@@ -38,15 +34,10 @@ class Form{
             player.update();
             player.updateCount(playerCount);
             this.greeting.html("Hello " + player.name)
-            this.greeting.position(400,250);
+            this.greeting.position(700,250);
             this.greeting.style('color', 'white');
             this.greeting.style('font-size', '100px');
         });
 
-        //
-        this.reset.mousePressed(()=> {
-            player.updateCount(0);
-            game.update(0);
-        })
     }
 }
